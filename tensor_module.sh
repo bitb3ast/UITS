0242b6431b7960f34d4a01a4e121c9b8edf87a80
bitb3ast <jerrinsuresh@gmail.com>
2017-03-23 11:51:36 -0400

    fix: actual file
    the original form of /environment in the container

--- tensor_module.sh
+++ tensor_module.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+# Define any environment init code here
 
+if test -z "$SINGULARITY_INIT"; then
+    PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/bin:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/libnvvp
+    PS1="Singularity.$SINGULARITY_CONTAINER> $PS1"
+    SINGULARITY_INIT=1
+    CUDATOOLKIT_HOME=$CUDATOOLKIT_HOME:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1
+    CRAY_CUDATOOLKIT_VERSION=$CRAY_CUDATOOLKIT_VERSION:7.5.18-1.0502.10743.2.1
+    CRAY_CUDATOOLKIT_DIR=$CRAY_CUDATOOLKIT_DIR:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1
+    MANPATH=$MANPATH:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/doc/man
+    PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/lib64/pkgconfig
+    PE_PKGCONFIG_LIBS=$PE_PKGCONFIG_LIBS:cray-cudatoolkit
+    CRAY_LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/lib64:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/extras/CUPTI/lib64
+    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/lib64:/opt/cray/nvidia/352.68-1_1.0502.2451.1.1.gem/lib64
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cray/nvidia/352.68-1_1.0502.2451.1.1.gem/lib64
-export CUDATOOLKIT_HOME=$CUDATOOLKIT_HOME:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1
-export CRAY_CUDATOOLKIT_VERSION=$CRAY_CUDATOOLKIT_VERSION:7.5.18-1.0502.10743.2.1
-export CRAY_CUDATOOLKIT_DIR=$CRAY_CUDATOOLKIT_DIR:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1
-export MANPATH=$MANPATH:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/doc/man
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/lib64/pkgconfig
-export PE_PKGCONFIG_LIBS=$PE_PKGCONFIG_LIBS:cray-cudatoolkit
-export CRAY_LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/lib64:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/extras/CUPTI/lib64
-export PATH=$PATH:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/bin:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/libnvvp
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nvidia/cudatoolkit7.5/7.5.18-1.0502.10743.2.1/lib64:/opt/cray/nvidia/352.68-1_1.0502.2451.1.1.gem/lib64
-
+    export PATH PS1 SINGULARITY_INIT CUDATOOLKIT_HOME CRAY_CUDATOOLKIT_VERSION CRAY_CUDATOOLKIT_DIR MANPATH PKG_CONFIG_PATH PE_PKGCONFIG_LIBS CRAY_LD_LIBRARY_PATH LD_LIBRARY_PATH
+fi
